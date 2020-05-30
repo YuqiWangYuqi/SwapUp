08f036de2257ca7f134b336a56c647f52cbdfe44
Yuqi Wang <wangy34@seattleu.edu>
2020-05-30 12:01:58 -0700

    code comments added in product.cs

--- SwapUp/Model/Product.cs
+++ SwapUp/Model/Product.cs
@@ -7,15 +7,20 @@ using System.Threading.Tasks;
 
 namespace SwapUp.Model
 {
+    /*
+     * this class is the product list attributes to list on the 
+     * main page as user uploaded items
+     */
     public class Product
     {
+        //item ID
         public string Id { get; set; }
-
+        //Item image from json file
         [JsonPropertyName("img")]
         public string Image { get; set; }
-
+        //Item referrence link
         public string Href { get; set; }
-
+        //Item title
         public string Title { get; set; }
     }
 }
