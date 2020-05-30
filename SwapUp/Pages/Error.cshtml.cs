ccae790e8cbeb85c9aa4fddcdec4e326d2c351fc
yang.421 <yang.421@tamu.edu>
2020-05-30 13:11:40 -0700

    Mofied the comments in Error.cshtml.cs file

--- SwapUp/Pages/Error.cshtml.cs
+++ SwapUp/Pages/Error.cshtml.cs
@@ -8,23 +8,22 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
 namespace SwapUp.Pages
-{
+{   /*
+     * error page c# class
+     */
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public class ErrorModel : PageModel
-    {   /*Generate property accessors*/
+    {   //Generate property accessors
         public string RequestId { get; set; }
-        /*ShowRequestId*/
+        //ShowRequestId
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-
-
-        /*Creates a logger, ILogger<AboutModel>, which uses a log category of the fully qualified name of the type AboutModel*/
+        //Creates a logger, ILogger<AboutModel>, which uses a log category of the fully qualified name of the type AboutModel
         private readonly ILogger<ErrorModel> _logger;
-
         public ErrorModel(ILogger<ErrorModel> logger)
         {
             _logger = logger;
         }
-        /*OnGet methood*/
+        //OnGet methood
         public void OnGet()
         {
             RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
