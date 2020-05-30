a2733539f95a0a8c58cc71daa428906874413dce
yang.421 <yang.421@tamu.edu>
2020-05-30 13:07:59 -0700

    Added the comments to Error.cshtml.cs file

--- SwapUp/Pages/Error.cshtml.cs
+++ SwapUp/Pages/Error.cshtml.cs
@@ -11,18 +11,20 @@ namespace SwapUp.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public class ErrorModel : PageModel
-    {
+    {   /*Generate property accessors*/
         public string RequestId { get; set; }
-
+        /*ShowRequestId*/
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
+
+        /*Creates a logger, ILogger<AboutModel>, which uses a log category of the fully qualified name of the type AboutModel*/
         private readonly ILogger<ErrorModel> _logger;
 
         public ErrorModel(ILogger<ErrorModel> logger)
         {
             _logger = logger;
         }
-
+        /*OnGet methood*/
         public void OnGet()
         {
             RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
